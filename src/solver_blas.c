5e76de7c54f2b074ec368f92d9a88f0f2ecb84c4
Diana Cismaru <cismaru_diana02@yahoo.com>
2024-05-18 17:06:36 +0300

    Add comments to BLAS file

--- src/solver_blas.c
+++ src/solver_blas.c
@@ -1,57 +1,48 @@
 /*
- * Tema 2 ASC
+ * Tema 3 ASC
  * 2024 Spring
  */
 #include "utils.h"
 #include <string.h>
 #include <cblas.h>
 
+// Allocate memory for a matrix of size N x N
 double* allocate_matrix(int N) {
 	double *A = (double*) calloc(N * N, sizeof(double));
+
+	// Safety check
 	if (!A) {
 		printf("Memory allocation failed\n");
 		exit(1);
 	}
+
 	return A;
 }
 
-double* add_matrices(int N, double *A, double *B, double *res) {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			res[i * N + j] = A[i * N + j] + B[i * N + j];
-		}
-	}
-
-	return res;
-}
-
-/* 
- * Add your BLAS implementation here
- */
 double* my_solver(int N, double *A, double *B) {
-	// DGEMM - C = alpha * A * B + beta * C, matrice normale
-	// DTRMM - B = alpha * A * B, matrice triunghiulare
-	// At * B, A superior triangular
-
-	// Copiez in AtB matricea B, pentru a putea stoca si rezultatul tot aici
+	// Create a copy of B in AtB, to store the result of At * B
 	double *AtB = allocate_matrix(N);
 	memcpy(AtB, B, N * N * sizeof(double));
 	cblas_dtrmm(CblasRowMajor, CblasLeft, CblasUpper, CblasTrans, CblasNonUnit,
 				N, N, 1, A, N, AtB, N);
 
+	// Create a copy of B in BA, to store the result of B * A
 	double *BA = allocate_matrix(N);
-	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, N, N, 1, B, N, A,
-				N, 0, BA, N);
+	memcpy(BA, B, N * N * sizeof(double));
+	cblas_dtrmm(CblasRowMajor, CblasRight, CblasUpper, CblasNoTrans, CblasNonUnit,
+				N, N, 1, A, N, BA, N);
 
-	double *paranthesis = add_matrices(N, AtB, BA, allocate_matrix(N));
+	// Add the vectors AtB and BA and store the result in BA
+	cblas_daxpy(N * N, 1, AtB, 1, BA, 1);
 
+	// Multiply BA (currently containing At x B + B x A) with Bt
 	double *result = allocate_matrix(N);
-	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, 1, paranthesis, 
+	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, 1, BA, 
 				N, B, N, 0, result, N);
 
+	// Free the allocated memory
 	free(AtB);
 	free(BA);
-	free(paranthesis);
 
 	return result;
 }
